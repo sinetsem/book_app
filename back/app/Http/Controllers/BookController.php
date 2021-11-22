12746a5c5242fb09e2f3bfe564717a267d0f314c
sinet.sem <sinet.sem@student.passerellesnumeriques.org>
2021-11-22 22:55:36 +0700

    push code

---
+++ back/app/Http/Controllers/BookController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAllBook()
+    {
+        //
+        return Book::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function createBook(Request $request)
+    {
+        //
+        $book = new Book();
+        $book->title = $request->title;
+        $book->description = $request -> description;
+
+        $book->save();
+
+        return response()->json(['message'=>'Book Created Successfully','book'=>$book],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getOneBook($id)
+    {
+        //
+        return Book::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateBook(Request $request, $id)
+    {
+        //
+        $book = Book::findOrFail($id);
+        $book->title = $request->title;
+        $book->description = $request -> description;
+
+        $book->save();
+
+        return response()->json(['message'=>'Book Updated Successfully','book'=>$book],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteBook($id)
+    {
+        //
+        return Book::destroy($id);
+    }
+}
